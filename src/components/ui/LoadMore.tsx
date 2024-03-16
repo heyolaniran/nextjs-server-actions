2c48af11e921523b9d388294a1a39d21d1feb248
heyolaniran <olaniran.abd@gmail.com>
2024-03-16 18:06:48 +0100

    fix: dynamics pagination render on scroll

--- src/components/ui/LoadMore.tsx
+++ src/components/ui/LoadMore.tsx
@@ -1,10 +1,45 @@
+'use client'
 import Image from "next/image"
+import { useEffect, useState } from "react";
+
+import { useInView } from "react-intersection-observer" ; 
+import { AnimeCardProp } from "../../../types/AnimeCardProp";
+import { fetchAnime } from "@/action";
+import AnimeCard from "../AnimeCard";
+
+let page = 2 ; 
+
 export default function LoadMore() {
+
+    const [ref, inView] = useInView() ; 
+
+    const [data,  setData] = useState<AnimeCardProp[]>([]) ; 
+
+    useEffect(() => {
+
+        if(inView) {
+            fetchAnime(page)
+            .then((res) => {
+                setData([...data, ...res]); 
+
+                page++ ; 
+            })
+        }
+
+    }, [inView])
+
 
     return (
         <>
+            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+
+                {data.map((item : AnimeCardProp , index : number ) => (
+                    <AnimeCard key={item.id} anime={item} index={index} /> 
+                ))}
+
+            </section>
             <section className="flex mt-2 justify-center items-center w-full">
-                <div>
+                <div ref={ref}>
                     <Image src={'./spinner.svg'} height={64} width={64} className="object-contain" alt="spin" /> 
                 </div>
             </section>
