2dabf236f6ba46d7ec99c3fd9946f10b2c5c556a
heyolaniran <olaniran.abd@gmail.com>
2024-03-16 18:05:45 +0100

    fix: render card limit

--- src/action.ts
+++ src/action.ts
@@ -2,7 +2,7 @@
 
 export const fetchAnime = async (page : number) => {
 
-    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=10&order=popularity`) ; 
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity`) ; 
 
     const data = await response.json() ; 
 
